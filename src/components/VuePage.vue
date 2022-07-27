9e8b50773f7de58d20fa916344507d7a7387630a
Josh Pennington <josh@jpennington.net>
2022-07-26 22:43:57 -0400

    Initial Commit

---
+++ src/components/VuePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>
+            <slot name="header">??header??</slot>
+        </h1>
+        <main>
+            <slot name="content">??content??</slot>
+        </main>
+    </div>
+</template>
